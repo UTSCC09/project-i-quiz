27c68097e3151c882fe97c170d3dd698569146ba
Archie Liu <archielhd@gmail.com>
2023-10-19 11:38:01 -0400

    Use tab for small size screen

--- client/src/pages/CourseDashboard.jsx
+++ client/src/pages/CourseDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import NavBar from "components/page_components/NavBar";
 import CourseCard from "components/page_components/CourseCard";
@@ -12,17 +12,46 @@ const quizData = QuizArrMock_available;
 const quizData2 = QuizArrMock_upcoming;
 
 export default function CourseDashboard() {
+  const quizSectionRef = useRef(null);
+  const courseSectionRef = useRef(null);
+
+  const [selectedTab, _setSelectedTab] = useState("quizzes");
+  function setSelectedTab(selection) {
+    _setSelectedTab(selection);
+    quizSectionRef.current.classList.toggle("hidden");
+    courseSectionRef.current.classList.toggle("hidden");
+  }
+
   return (
     <>
       <NavBar />
       <div className="min-h-screen w-screen bg-gray-100">
-        <main className="h-fit px-8 gap-y-8 gap-x-[4%] md:px-24 w-full flex flex-col lg:flex-row py-36">
-          <div className="lg:w-[35%] flex flex-col gap-8">
+        <main className="h-fit px-8 gap-y-8 gap-x-[4%] md:px-24 w-full flex flex-col lg:flex-row py-32 sm:py-36">
+          <div className="flex lg:hidden bg-gray-200 rounded-lg w-full justify-between">
+            <label className="cursor-pointer w-[49%]">
+              <input type="radio" name="tab" className="hidden peer" checked={selectedTab === "quizzes"}
+                onChange={() => setSelectedTab("quizzes")} />
+              <div
+                className="rounded-lg p-2 text-sm peer-checked:font-medium text-center  peer-checked:bg-white text-slate-600 peer-checked:border peer-checked:shadow-sm w-full"
+              >
+                Quizzes
+              </div>
+            </label>
+            <label className="cursor-pointer w-[49%]">
+              <input type="radio" id="courses" name="tab" className="hidden peer" checked={selectedTab === "courses"} onChange={() => setSelectedTab("courses")} />
+              <div
+                className="rounded-lg p-2 text-sm peer-checked:font-medium text-center  peer-checked:bg-white text-slate-600 peer-checked:border peer-checked:shadow-sm w-full"
+              >
+                Courses
+              </div>
+            </label>
+          </div>
+          <div ref={quizSectionRef} className="lg:w-[35%] flex flex-col gap-8 lg:flex">
             <QuizListSection sectionName="Available Quizzes" quizDataArr={quizData.quizList} />
             <QuizListSection sectionName="Upcoming Quizzes" quizDataArr={quizData2.quizList} />
           </div>
-          <div className="lg:w-[65%]">
-            <div className="text-slate-600 font-medium mb-6">My Courses</div>
+          <div ref={courseSectionRef} className="hidden lg:block lg:w-[65%]">
+            <div className="text-slate-600 font-medium mb-4">My Courses</div>
             <div className="flex flex-wrap gap-x-[4%] 2xl:gap-x-[3.5%] gap-y-6 md:gap-y-8 h-fit lg:w1-fit">
               {
                 courseData.courseList.map((courseObject, idx) => {
@@ -63,7 +92,7 @@ function QuizListSection({ sectionName, quizDataArr }) {
           <input name="showToggle" type="checkbox" className="peer hidden" checked={!show} readOnly />
           <div className="font-medium">{sectionName}</div>
           <svg className="h-3.5 transition ease-in-out duration-200 peer-checked:rotate-180" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path clipRule="evenodd" fillRule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z" stroke="currentColor" strokeWidth="2"></path>
+            <path clipRule="evenodd" fillRule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z" stroke="currentColor" strokeWidth="1"></path>
           </svg>
         </div>
         <AnimatePresence initial={false}> {show &&
--- client/src/pages/QuizPage.jsx
+++ client/src/pages/QuizPage.jsx
@@ -27,19 +27,19 @@ const QuizPage = () => {
       <NavBar />
       <form>
         <div className="w-screen flex justify-center py-36 bg-gray-100">
-          <div className="w-full px-8 md:w-[48rem] flex flex-col items-center">
+          <div className="w-full px-4 md:w-[48rem] flex flex-col items-center">
             {
               QuizMock.questions.map((questionObj, idx) => {
                 return (
                   <div
-                    className="h-fit w-full m-5 flex flex-col border drop-shadow-sm bg-white shadow-gray-150 rounded-md py-12"
+                    className="h-fit w-full my-4 flex flex-col md:border drop-shadow-sm bg-white shadow-gray-150 rounded-md py-12"
                     key={questionObj.qid}
                   >
                     <span className="font-bold text-sm uppercase ml-12 text-gray-500 mb-4">
                       Question {idx + 1} / {QuizMock.questions.length}
                     </span>
                     <div className="border-b h-0 mb-6 mx-10"></div>
-                    <div className="mx-12">
+                    <div className="mx-10">
                       <QuestionWrapper
                         questionObject={questionObj}
                         savedAnswer={savedAnswers[idx]}
