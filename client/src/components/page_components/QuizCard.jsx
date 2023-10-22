e697aabbb64f86cba6e157ce1210549e89d130cc
Archie Liu <archie.liu@mail.utoronto.ca>
2023-10-22 12:37:34 -0400

    [IQZ-20] Course & quiz dashboard UI (#19)
    * Add course dashboard
    
    * Remove hero icon dependency
    
    * Refactor icon button components
    
    * Miscellaneous fixes
    
    * Support responsive design
    
    * Add QuizCard
    
    * Fix responsive layout
    
    * Fix superfluous right margin on 2xl screen
    
    * Data embedding
    
    * Resolve ESLint warning
    
    * Revert change in ClozeQuestion
    
    * Export mocks to JSON files
    
    * Extract page components
    
    * Fix unexpected whitespace at end of the dashboard
    
    * Add CoursePage
    
    * Update mock data
    
    * Allow quiz card to resize; add submitted label
    
    * Add NavBar to quiz page
    
    * Add animation to quiz lists
    
    * Use tab for small size screen
    
    * Add custom dropdown filter
    
    * Fix width issue with scroll bar
    
    * Extract accordion component
    
    * Extract dropdown component
    
    * Rename quiz data mocks
    
    * Rename section name to "Active Courses"
    
    * Rename from CourseDashboard to Dashboard
    
    * Remove 3 columns view for 2xl screen size
    
    * Redirect to dashboard after login
    
    * Scroll to top on redirects (React router v6 bug?)
    
    * Give quiz card full width

---
+++ client/src/components/page_components/QuizCard.jsx
@@ -0,0 +1,86 @@
+
+import React from "react";
+import Badge from "components/elements/Badge.jsx";
+import colors from "tailwindcss/colors";
+import { Link } from "react-router-dom";
+
+function getQuizState(quizObject) {
+  const startTime = new Date(quizObject.startTime);
+  const endTime = new Date(quizObject.endTime);
+  const currentTime = new Date();
+
+  if (startTime > currentTime) {
+    return "upcoming"
+  }
+  else if (endTime > currentTime) {
+    return "available";
+  }
+  else {
+    return "closed";
+  }
+}
+
+function getFormattedDateStr(time) {
+  const dateFormatOptions = { month: "short", day: "numeric" };
+  const timeFormatOptions = { hour: "numeric", minute: "numeric" };
+  return time.toLocaleDateString(undefined, dateFormatOptions) + " at " + time.toLocaleTimeString(undefined, timeFormatOptions);
+}
+
+// quizState: available, upcoming, closed
+export default function QuizCard({ quizObject }) {
+  const quizName = quizObject.quizName;
+  const courseCode = quizObject.courseCode;
+  const startTime = new Date(quizObject.endTime);
+  const endTime = new Date(quizObject.endTime);
+  const accentColor = quizObject.accentColor;
+  const submitted = quizObject.submitted;
+
+  const quizState = getQuizState(quizObject);
+
+  const startTimeStr = getFormattedDateStr(startTime);
+  const endTimeStr = getFormattedDateStr(endTime);
+
+  let quizAvailabilityPrompt, isAvailable;
+
+  switch (quizState) {
+    case "available":
+      quizAvailabilityPrompt = "Available untill " + startTimeStr;
+      isAvailable = true;
+      break;
+    case "upcoming":
+      quizAvailabilityPrompt = "Unlocks on " + startTimeStr;
+      isAvailable = false;
+      break;
+    case "closed":
+      quizAvailabilityPrompt = "Closed on " + endTimeStr;
+      isAvailable = false;
+      break;
+    default:
+      break;
+  }
+  return (
+    <>
+      <Link to="/quiz/0" className="h-fit w-full rounded border-l-[12px] shadow shadow-gray-200 group cursor-pointer" style={{ borderLeftColor: accentColor, pointerEvents: isAvailable ? "auto" : "none", opacity: isAvailable ? 1 : 0.5 }}>
+        <div className="w-full py-4 md:py-6 h-fit bg-white group-hover:bg-gray-100 rounded-r border px-4 md:px-6 shrink-0 flex items-center transition-all">
+          <div className="flex-col justify-center items-start inline-flex">
+            <div className="items-center gap-2.5 inline-flex">
+              <div className="md:text-lg font-semibold">
+                {quizName}
+              </div>
+              {isAvailable && !submitted &&
+                <div className="w-2 h-2 shrink-0 rounded-full" style={{ backgroundColor: accentColor }}></div>
+              }
+              <Badge label={courseCode} accentColor={accentColor} />
+              {submitted &&
+                <Badge label={"submitted"} accentColor={colors.green[600]} />
+              }
+            </div>
+            <div className="text-gray-500 text-xs font-normal">
+              {quizAvailabilityPrompt}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+}
