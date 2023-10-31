7c55fde3fdc928b1007784ec7bb4a4855ffc5fee
ericckzhou <ericckzhou@gmail.com>
2023-10-31 14:09:01 -0400

    Handled server connection errors

--- server/server.js
+++ server/server.js
@@ -13,13 +13,23 @@ dotenv.config();
 
 // Database
 global.mongoose = mongoose;
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-mongoose.connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
+const connectDB = async() => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      keepAlive: true
+    });
+    mongoose.connection.once("open", () => {
+      console.log("MongoDB database connection established successfully");
+    });
+  }
+  catch (err){
+    console.log("Failed to connect to mongoDB.");
+    console.log(err);
+  }
+}
+connectDB();
 
 // Initialize server
 const app = express();
