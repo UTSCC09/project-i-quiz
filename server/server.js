9cd4dd564ae5c75c87965dd5d5ca13e36998f04c
ericckzhou <ericckzhou@gmail.com>
2023-11-02 05:17:12 -0400

    Removed keepAlive option

--- server/server.js
+++ server/server.js
@@ -17,8 +17,7 @@ const connectDB = async() => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      keepAlive: true
+      useUnifiedTopology: true
     });
     mongoose.connection.once("open", () => {
       console.log("MongoDB database connection established successfully");
