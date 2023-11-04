8221e2932bd31dfd473f8f20e69c06fe5150bbb9
ericckzhou <ericckzhou@gmail.com>
2023-11-04 08:57:51 -0400

    Added email verification transporter

---
+++ server/utils/emailVerification.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    secureConnection: false, 
+    port: 587, 
+    auth: {
+      user: process.env.AUTH_EMAIL,
+      pass: process.env.AUTH_PASS
+    }
+});
+
+const sendEmailConfirmation = function(user){
+    let backendUrl = "http://localhost:" + process.env.PORT + "/";
+    let mailOptions = {
+        from: process.env.AUTH_EMAIL,
+        to: user.email,
+        subject: 'iQuiz Email Verification',
+        html: `<p>Verify your iQuiz account below by clicking on the link!</p>
+        <p><a href=${backendUrl + "api/users/verify/" + user._id + "/" + user.confirmationCode}>Click here.</p>`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.log("Failed to send email", error);
+    } else {
+        console.log("Email sent successfully!");
+    }
+    });
+}
+
+export default sendEmailConfirmation;
