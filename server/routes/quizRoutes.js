d9d6249a3bc1778bdecf021733fa58b90a1504bd
4meyDam1e <amey.damle@mail.utoronto.ca>
2023-11-06 18:53:39 -0500

    Added get endpoints for getting quizzes

--- server/routes/quizRoutes.js
+++ server/routes/quizRoutes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import {
-  createQuiz
+  createQuiz,
+  getQuiz,
+  getQuizzesForInstructedCourse,
+  getQuizzesForEnrolledCourse
 } from "../controllers/quizController.js";
 import protect from "../middleware/authMiddleware.js";
 
@@ -8,5 +11,14 @@ const router = Router();
 
 router.route("/")
   .post(protect, createQuiz);
+
+router.route("/:quizId")
+  .get(protect, getQuiz)
+
+router.route("/course/instructed/:courseId")
+  .get(protect, getQuizzesForInstructedCourse);
+
+  router.route("/course/enrolled/:courseId")
+  .get(protect, getQuizzesForEnrolledCourse);
 
 export default router;
