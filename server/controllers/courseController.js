8a4ba92535374083fbea54fc0f3c5790cec22d07
Archie Liu <archie.liu@mail.utoronto.ca>
2023-11-08 18:44:24 -0500

    [IQZ-36] Add APIs to support course accent color and access code  (#31)
    * Add courseSemester to the response
    
    * Support accentColor
    
    * Check existing course only within the instructor
    
    * Fix setAccentColor not working
    
    * Add get course enroll info API
    
    * Add getEnrolledCourse API
    
    * Rename route path to follow snake case
    
    * Add set access code API
    
    * Error handling for fetchFormattedCourse
    
    * Address comments
    
    * Fix crashing when getEnrolledCourses fails
    
    * Fix accent color not returned for instructor

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -7,32 +7,41 @@ import formatMessage from "../utils/utils.js";
 //@desc   Allow instructor to create a course
 //@access Private
 const createCourse = asyncHandler(async (req, res) => {
-  const { courseCode, courseName, semester, numOfSessions } = req.body;
+  const {
+    courseCode,
+    courseName,
+    courseSemester,
+    numOfSessions,
+    accessCode,
+    accentColor,
+  } = req.body;
+
+  //Verify all fields exist
+  if (!courseCode || !courseName || !courseSemester || !numOfSessions) {
+    return res.status(400).json(formatMessage(false, "Missing fields"));
+  }
 
   //Check if valid user
   const instructor = await User.findOne({ email: req.session.email });
   if (!instructor) {
     return res.status(400).json(formatMessage(false, "Invalid user"));
-  }
-  else if (instructor.type !== "instructor") {
+  } else if (instructor.type !== "instructor") {
     return res.status(400).json(formatMessage(false, "Invalid user type"));
   }
 
-  //Verify all fields exist
-  if (!courseCode || !courseName || !semester || !numOfSessions) {
-    return res.status(400).json(formatMessage(false, "Missing fields"));
-  }
-
-  //Check if there is a pre-existing course
-  const existingCourse = await Course.findOne(
-    {$and: [
-      {$or: [
-        { courseCode: courseCode },
-        { courseName: courseName }
-      ]},
-      { semester: semester }
-    ]});
-  if (existingCourse) {
+  //Check if there is a pre-existing course in the instructor's course list
+  const existingCourse = await Course.findOne({
+    $and: [
+      { $or: [{ courseCode: courseCode }, { courseName: courseName }] },
+      { courseSemester: courseSemester },
+    ],
+  });
+  if (
+    existingCourse &&
+    instructor.courses.findIndex((course) =>
+      course.courseId.equals(existingCourse._id)
+    ) !== -1
+  ) {
     return res.status(400).json(formatMessage(false, "Course already exists"));
   }
 
@@ -42,7 +51,7 @@ const createCourse = asyncHandler(async (req, res) => {
     sessions.push({
       sessionNumber: i + 1,
       students: [],
-      enrollment: 0
+      enrollment: 0,
     });
   }
 
@@ -50,17 +59,68 @@ const createCourse = asyncHandler(async (req, res) => {
   const course = await Course.create({
     courseCode: courseCode,
     courseName: courseName,
-    semester: semester,
+    courseSemester: courseSemester,
     instructor: instructor._id,
-    sessions: sessions
+    sessions: sessions,
+    accessCode: accessCode,
   });
   if (course) {
-    instructor.courses.push(course._id);
+    instructor.courses.push({
+      courseId: course._id,
+      accentColor: accentColor,
+    });
     await instructor.save();
-    return res.status(200).json(formatMessage(true, "Course created successfully"));
+    return res
+      .status(200)
+      .json(formatMessage(true, "Course created successfully"));
   }
   else {
     return res.status(400).json(formatMessage(false, "Course creation failed"));
+  }
+});
+
+//@route  POST api/courses/access_code
+//@desc   Allow instructor to update access code for a course
+//@access Private
+const setAccessCode = asyncHandler(async (req, res) => {
+  const { courseId, accessCode } = req.body;
+
+  // Verify all fields exist
+  if (!courseId || !accessCode) {
+    return res.status(400).json(formatMessage(false, "Missing fields"));
+  }
+
+  // Check if valid user
+  const instructor = await User.findOne({ email: req.session.email });
+  if (!instructor) {
+    return res.status(400).json(formatMessage(false, "Invalid user"));
+  } else if (instructor.type !== "instructor") {
+    return res.status(400).json(formatMessage(false, "Invalid user type"));
+  }
+
+  const courseObject = await Course.findById(courseId);
+
+  // Check if valid course
+  if (!courseObject) {
+    return res.status(400).json(formatMessage(false, "Invalid courseId"));
+  }
+
+  // Check if the user is the course instructor
+  if (!courseObject.instructor.equals(instructor._id)) {
+    return res.status(400).json(formatMessage(false, "Access denied"));
+  }
+
+  const updatedCourse = await Course.updateOne(
+    { _id: courseId },
+    { $set: { accessCode: accessCode } }
+  );
+  if (updatedCourse) {
+    await courseObject.save();
+    return res
+      .status(200)
+      .json(
+        formatMessage(true, "Access code updated successfully", accessCode)
+      );
   }
 });
 
@@ -77,46 +137,32 @@ const getMyInstructedCourses = asyncHandler(async (req, res) => {
   }
 
   // Fetch courses in parallel
-  const instructedCoursesPromises = instructor.courses.map(async (courseId) => {
-    const course = await Course.findById(courseId);
-    return fetchFormattedCourse(course, instructor);
+  const instructedCoursesPromises = instructor.courses.map(async (course) => {
+    const courseObject = await Course.findById(course.courseId);
+    return fetchFormattedCourse(courseObject, course.accentColor, instructor);
   });
 
   const instructedCourses = await Promise.all(instructedCoursesPromises);
 
   return res
     .status(200)
-    .json(formatMessage(true, "Courses retrieved successfully", instructedCourses));
+    .json(
+      formatMessage(true, "Courses retrieved successfully", instructedCourses)
+    );
 });
 
 //@route  GET api/courses/enrolled
 //@desc   Retrieve all courses signed in student is enrolled in
 //@access Private
 const getMyEnrolledCourses = asyncHandler(async (req, res) => {
-  //Check if valid user
-  const student = await User.findOne({ email: req.session.email });
-  if (!student) {
-    return res.status(400).json(formatMessage(false, "Invalid user"));
-  }
-  else if (student.type !== "student") {
-    return res.status(400).json(formatMessage(false, "Invalid user type"));
-  }
-
-  const enrolledCourses = [];
-  for (let i = 0; i < student.courses.length; i++) {
-    const course = await Course.findById(student.courses[i]);
-    const instructor = await User.findById(course.instructor);
-    //Students should only be able to see the some fields
-    enrolledCourses.push({
-      courseCode: course.courseCode,
-      courseName: course.courseName,
-      semester: course.semester,
-      instructor: instructor.firstName + " " + instructor.lastName
-    });
-  }
+  const enrolledCourses = await getEnrolledCourses(req.session.email);
 
   //Retrieve courses
-  return res.status(200).json(formatMessage(true, "Courses retrieved successfully", enrolledCourses));
+  return res
+    .status(200)
+    .json(
+      formatMessage(true, "Courses retrieved successfully", enrolledCourses)
+    );
 });
 
 //@route  GET api/courses
@@ -131,7 +177,7 @@ const getAllCourses = asyncHandler(async (req, res) => {
     for (let j = 0; j < courses[i].sessions.length; j++) {
       formattedSessions.push({
         sessionNumber: courses[i].sessions[j].sessionNumber,
-        enrollment: courses[i].sessions[j].enrollment
+        enrollment: courses[i].sessions[j].enrollment,
       });
     }
     formattedCourses.push({
@@ -139,17 +185,43 @@ const getAllCourses = asyncHandler(async (req, res) => {
       courseName: courses[i].courseName,
       instructorName: instructor.firstName + " " + instructor.lastName,
       instructorEmail: instructor.email,
-      sessions: formattedSessions      
+      sessions: formattedSessions,
     });
   }
-  return res.status(200).json(formatMessage(true, "Courses retrieved successfully", formattedCourses));
+  return res
+    .status(200)
+    .json(
+      formatMessage(true, "Courses retrieved successfully", formattedCourses)
+    );
+});
+
+//@route  GET api/courses/enrolled/:courseId
+//@desc   Retrieve all courses
+//@access Private
+const getEnrolledCourse = asyncHandler(async (req, res) => {
+  const courseId = req.params.courseId;
+  const enrolledCourses = await getEnrolledCourses(req.session.email);
+
+  const course = enrolledCourses.find(
+    (course) => course.courseId.toString() === courseId
+  );
+
+  if (!course) {
+    return res
+      .status(400)
+      .json(formatMessage(false, "Student not enrolled in the course"));
+  }
+
+  return res
+    .status(200)
+    .json(formatMessage(true, "Course retrieved successfully", course));
 });
 
 //@route  POST api/courses/enroll
 //@desc   Add student to their desired course session
 //@access Private
 const enrollInCourse = asyncHandler(async (req, res) => {
-  const { courseId, sessionNumber } = req.body;
+  const { courseId, sessionNumber, accentColor } = req.body;
 
   //Check if valid user
   const student = await User.findOne({ email: req.session.email});
@@ -168,37 +240,91 @@ const enrollInCourse = asyncHandler(async (req, res) => {
   //Check if valid course and session
   const course = await Course.findById(courseId);
   if (!course || !course.sessions[sessionNumber - 1]) {
-    return res.status(400).json(formatMessage(false, "Invalid course or session"));
+    return res
+      .status(400)
+      .json(formatMessage(false, "Invalid course or session"));
   }
 
   //Check if student is already enrolled in the specified course
-  if (student.courses.includes(courseId)) {
+  if (
+    student.courses.findIndex((course) => course.courseId == courseId) !== -1
+  ) {
     //Check if student is already enrolled in the specified session
     if (course.sessions[sessionNumber - 1].students.includes(student._id)) {
-      return res.status(400).json(formatMessage(false, "Student already enrolled in course"));
+      return res
+        .status(400)
+        .json(formatMessage(false, "Student already enrolled in course"));
     }
 
     //Remove student from the other session they're enrolled in
     for (let i = 0; i < course.sessions.length; i++) {
       if (course.sessions[i].students.includes(student._id)) {
-        course.sessions[i].students = course.sessions[i].students.filter(studentId => studentId.toString() !== student._id.toString());
+        course.sessions[i].students = course.sessions[i].students.filter(
+          (studentId) => studentId.toString() !== student._id.toString()
+        );
         course.sessions[i].enrollment--;
         await course.save();
         break;
       }
     }
-  }
-  else {
-    student.courses.push(courseId);
+  } else {
+    student.courses.push({ courseId: courseId, accentColor: accentColor });
     await student.save();
+    //Enroll student
+    course.sessions[sessionNumber - 1].students.push(student._id);
+    course.sessions[sessionNumber - 1].enrollment++;
+    await course.save();
+
+    return res.status(200).json(
+      formatMessage(true, "Student enrolled successfully", {
+        courseCode: course.courseCode,
+      })
+    );
+  }
+});
+
+//@route  GET api/courses/enroll_info/:accessCode
+//@desc   Get courseId and [sessionNumber] given accessCode
+//@access Private
+const getCourseEnrollInfo = asyncHandler(async (req, res) => {
+  const { accessCode } = req.params;
+
+  //Check if valid user
+  const student = await User.findOne({ email: req.session.email });
+  if (!student) {
+    return res.status(400).json(formatMessage(false, "Invalid user"));
+  } else if (student.type !== "student") {
+    return res.status(400).json(formatMessage(false, "Invalid user type"));
   }
 
-  //Enroll student
-  course.sessions[sessionNumber - 1].students.push(student._id);
-  course.sessions[sessionNumber - 1].enrollment++;
-  await course.save();
+  //Check if valid course and session
+  const course = await Course.findOne({ accessCode: accessCode });
+  if (!course) {
+    return res
+      .status(400)
+      .json(formatMessage(false, "Invalid course access code"));
+  }
 
-  return res.status(200).json(formatMessage(true, "Student enrolled successfully"));
+  //Check if student is already enrolled in the specified course
+  if (
+    student.courses.findIndex((studentCourse) =>
+      studentCourse.courseId.equals(course._id)
+    ) !== -1
+  ) {
+    return res
+      .status(400)
+      .json(
+        formatMessage(
+          false,
+          `You have already enrolled in ${course.courseCode} ${course.courseSemester}`
+        )
+      );
+  }
+  return res.status(200).json({
+    success: true,
+    courseId: course._id,
+    sessions: course.sessions.map((session) => session.sessionNumber),
+  });
 });
 
 //@route  POST api/courses/drop
@@ -207,59 +333,147 @@ const enrollInCourse = asyncHandler(async (req, res) => {
 const dropCourse = asyncHandler(async (req, res) => {
   const { courseId } = req.body;
 
-  //Check if valid user
-  const student = await User.findOne({ email: req.session.email });
-  if (!student) {
-    return res.status(400).json(formatMessage(false, "Invalid user"));
-  }
-  else if (student.type !== "student") {
-    return res.status(400).json(formatMessage(false, "Invalid user type"));
-  }
-
-  //Verify all fields exist
+  // Verify all fields exist
   if (!courseId) {
     return res.status(400).json(formatMessage(false, "Missing fields"));
   }
 
-  //Check if valid course
+  // Check if valid user
+  const student = await User.findOne({ email: req.session.email });
+  if (!student) {
+    return res.status(400).json(formatMessage(false, "Invalid user"));
+  } else if (student.type !== "student") {
+    return res.status(400).json(formatMessage(false, "Invalid user type"));
+  }
+
+  // Check if valid course
   const course = await Course.findById(courseId);
   if (!course) {
     return res.status(400).json(formatMessage(false, "Invalid course"));
   }
 
-  //Check if student is enrolled in the specified course
-  if (student.courses.includes(courseId)) {
-    //Remove course from student's courses
-    student.courses = student.courses.filter(courseId => courseId.toString() !== course._id.toString());
-    await student.save();
+  // Remove course from student enrolled course list
+  const courseIndex = student.courses.findIndex(
+    (course) => course.courseId.toString() === courseId
+  );
+  if (courseIndex === -1) {
+    return res
+      .status(400)
+      .json(formatMessage(false, "Student not enrolled in the course"));
+  }
+  student.courses.splice(courseIndex, 1);
+  await student.save();
 
-    //Remove student from the course
-    for (let i = 0; i < course.sessions.length; i++) {
-      if (course.sessions[i].students.includes(student._id)) {
-        course.sessions[i].students = course.sessions[i].students.filter(studentId => studentId.toString() !== student._id.toString());
-        course.sessions[i].enrollment--;
-        await course.save();
-        break;
-      }
+  // Remove student from the course
+  for (let i = 0; i < course.sessions.length; i++) {
+    if (course.sessions[i].students.includes(student._id)) {
+      course.sessions[i].students = course.sessions[i].students.filter(
+        (studentId) => studentId.toString() !== student._id.toString()
+      );
+      course.sessions[i].enrollment--;
+      await course.save();
+      break;
     }
   }
-  else {
-    return res.status(400).json(formatMessage(false, "Student not enrolled in course"));
-  }
 
-  return res.status(200).json(formatMessage(true, "Student dropped successfully"));
+  return res
+    .status(200)
+    .json(formatMessage(true, "Student dropped successfully"));
 });
 
-// ------------------------------ Promise functions --------------------------------
-async function fetchFormattedCourse(course, instructor) {
-  const formattedSessionsPromises = course.sessions.map(async (session) => {
-    const formattedStudentsPromises = session.students.map(async (studentId) => {
-      const student = await User.findById(studentId);
-      return {
-        studentName: student.firstName + " " + student.lastName,
-        studentEmail: student.email,
-      };
+//@route  POST api/courses/accent_color
+//@desc   Set accent color for a specific course
+//@access Private
+const setAccentColor = asyncHandler(async (req, res) => {
+  const { courseId, accentColor } = req.body;
+
+  // Verify all fields exist
+  if (!courseId || !accentColor) {
+    return res.status(400).json(formatMessage(false, "Missing fields"));
+  }
+
+  // Check if valid user
+  const user = await User.findOne({ email: req.session.email });
+
+  // Check if valid course
+  const course = await Course.findById(courseId);
+  if (!course) {
+    return res.status(400).json(formatMessage(false, "Invalid courseId"));
+  }
+
+  // Check if the student is enrolled in the course
+  const courseIndex = user.courses.findIndex(
+    (course) => course.courseId.toString() === courseId
+  );
+
+  if (courseIndex === -1) {
+    return res
+      .status(400)
+      .json(
+        formatMessage(
+          false,
+          "The user has not enrolled in or instructed the course"
+        )
+      );
+  }
+  const updatedUser = await User.updateOne(
+    { _id: user._id, "courses.courseId": courseId },
+    { $set: { "courses.$.accentColor": accentColor } }
+  );
+  if (updatedUser) {
+    await user.save();
+    return res
+      .status(200)
+      .json(formatMessage(true, "Accent color set successfully", updatedUser));
+  } else {
+    return res
+      .status(500)
+      .json(formatMessage(false, "Could not update the color", updatedUser));
+  }
+});
+
+// ------------------------------ Helper functions ------------------------------
+
+async function getEnrolledCourses(studentEmail) {
+  const student = await User.findOne({ email: studentEmail });
+  if (!student || student.type !== "student") {
+    return [];
+  }
+
+  const enrolledCourses = [];
+
+  for (let i = 0; i < student.courses.length; i++) {
+    const course = await Course.findById(student.courses[i].courseId);
+    const instructor = await User.findById(course.instructor);
+    //Students should only be able to see the course code, course name, and instructor name
+    enrolledCourses.push({
+      courseId: course._id,
+      courseCode: course.courseCode,
+      courseName: course.courseName,
+      courseSemester: course.courseSemester,
+      instructor: instructor.firstName + " " + instructor.lastName,
+      accentColor: student.courses[i].accentColor,
     });
+  }
+  return enrolledCourses;
+}
+
+async function fetchFormattedCourse(course, accentColor, instructor) {
+  if (!course) {
+    return {};
+  }
+  const formattedSessionsPromises = course.sessions.map(async (session) => {
+    const formattedStudentsPromises = session.students.map(
+      async (studentId) => {
+        const student = await User.findById(studentId);
+        if (student) {
+          return {
+            studentName: student.firstName + " " + student.lastName,
+            studentEmail: student.email,
+          };
+        }
+      }
+    );
 
     const formattedStudents = await Promise.all(formattedStudentsPromises);
 
@@ -273,10 +487,11 @@ async function fetchFormattedCourse(course, instructor) {
   const formattedSessions = await Promise.all(formattedSessionsPromises);
 
   return {
-    _id: course._id,
+    courseId: course._id,
     courseCode: course.courseCode,
     courseName: course.courseName,
-    semester: course.semester,
+    courseSemester: course.courseSemester,
+    accentColor: accentColor,
     instructor: instructor.firstName + " " + instructor.lastName + " (Me)",
     sessions: formattedSessions,
     quizzes: course.quizzes
@@ -285,9 +500,13 @@ async function fetchFormattedCourse(course, instructor) {
 
 export {
   createCourse,
+  setAccessCode,
   getMyInstructedCourses,
   getMyEnrolledCourses,
   getAllCourses,
   enrollInCourse,
-  dropCourse
+  dropCourse,
+  setAccentColor,
+  getCourseEnrollInfo,
+  getEnrolledCourse,
 };
