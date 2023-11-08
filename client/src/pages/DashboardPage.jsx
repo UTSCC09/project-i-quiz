ff29dfa90634eb7955edeb190fd32ddd3bbd07c7
Archie Liu <archielhd@gmail.com>
2023-11-08 00:58:00 -0500

    Fix crashes when unauthorized

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -29,19 +29,22 @@ async function fetchEnrolledCourses() {
     method: "GET",
     withCredentials: true,
   })
-    .then((response) => {
+    .then(async (response) => {
+      if (response.status === 401) {
+        await fetch("/api/users/logout", { method: "GET" });
+      }
       return response.json();
     })
     .then((result) => {
       if (!result.success) {
         console.error(result.message);
-        return;
+        return [];
       }
       return result.payload;
     })
     .catch((err) => {
       console.error(err);
-    })
+    });
 }
 
 /* -- React Component -- */
@@ -63,9 +66,9 @@ export default function DashboardPage() {
 
   useEffect(() => {
     fetchEnrolledCourses().then((fetchedPayload) => {
-      activeCourseListSet(fetchedPayload)
+      activeCourseListSet(fetchedPayload);
     });
-  }, [activeCourseListSet])
+  }, [activeCourseListSet]);
 
   return (
     <>
@@ -78,69 +81,106 @@ export default function DashboardPage() {
           fetchEnrolledCourses().then((payload) => {
             activeCourseListSet(payload);
             setTimeout(() => {
-              toastMessageSet(courseCode + " has been added to your course list");
+              toastMessageSet(
+                courseCode + " has been added to your course list"
+              );
             }, 200);
           });
-        }} />
-      <NavBar additionalButtons={
-        <button className="btn-outline py-0 text-sm w-28 h-8 sm:h-10 shrink-0" onClick={() => enrollModalShowSet(true)}>Add course</button>
-      } />
+        }}
+      />
+      <NavBar
+        additionalButtons={
+          <button
+            className="btn-outline py-0 text-sm w-28 h-8 sm:h-10 shrink-0"
+            onClick={() => enrollModalShowSet(true)}
+          >
+            Add course
+          </button>
+        }
+      />
       <div className="min-h-screen w-full bg-gray-100">
         <main className="h-full px-8 gap-y-8 gap-x-[4%] md:px-24 w-full flex flex-col lg:flex-row py-32 sm:py-36">
           <div className="flex lg:hidden bg-gray-200 rounded-lg w-full justify-between">
             <label className="cursor-pointer w-[49%]">
-              <input type="radio" name="tab" className="hidden peer" checked={selectedTab === "quizzes"}
-                onChange={() => setSelectedTab("quizzes")} />
-              <div
-                className="rounded-lg p-2 text-sm peer-checked:font-medium text-center  peer-checked:bg-white text-slate-600 peer-checked:border peer-checked:shadow-sm w-full"
-              >
+              <input
+                type="radio"
+                name="tab"
+                className="hidden peer"
+                checked={selectedTab === "quizzes"}
+                onChange={() => setSelectedTab("quizzes")}
+              />
+              <div className="rounded-lg p-2 text-sm peer-checked:font-medium text-center  peer-checked:bg-white text-slate-600 peer-checked:border peer-checked:shadow-sm w-full">
                 Quizzes
               </div>
             </label>
             <label className="cursor-pointer w-[49%]">
-              <input type="radio" id="courses" name="tab" className="hidden peer" checked={selectedTab === "courses"} onChange={() => setSelectedTab("courses")} />
-              <div
-                className="rounded-lg p-2 text-sm peer-checked:font-medium text-center  peer-checked:bg-white text-slate-600 peer-checked:border peer-checked:shadow-sm w-full"
-              >
+              <input
+                type="radio"
+                id="courses"
+                name="tab"
+                className="hidden peer"
+                checked={selectedTab === "courses"}
+                onChange={() => setSelectedTab("courses")}
+              />
+              <div className="rounded-lg p-2 text-sm peer-checked:font-medium text-center  peer-checked:bg-white text-slate-600 peer-checked:border peer-checked:shadow-sm w-full">
                 Courses
               </div>
             </label>
           </div>
-          <div ref={quizSectionRef} className="lg:w-[35%] flex flex-col gap-8 lg:flex">
-            <Accordion sectionName="Available Quizzes"
+          <div
+            ref={quizSectionRef}
+            className="lg:w-[35%] flex flex-col gap-8 lg:flex"
+          >
+            <Accordion
+              sectionName="Available Quizzes"
               content={
-                <div className="flex flex-col gap-4 lg:gap-6">{
-                  getAvailableQuizzes().map((quizObject, idx) => {
-                    return <QuizCard quizObject={quizObject} key={idx} />
+                <div className="flex flex-col gap-4 lg:gap-6">
+                  {getAvailableQuizzes().map((quizObject, idx) => {
+                    return <QuizCard quizObject={quizObject} key={idx} />;
                   })}
                 </div>
               }
             />
-            <Accordion sectionName="Upcoming Quizzes"
+            <Accordion
+              sectionName="Upcoming Quizzes"
               content={
-                <div className="flex flex-col gap-4 lg:gap-6">{
-                  getUpcomingQuizzes().map((quizObject, idx) => {
-                    return <QuizCard quizObject={quizObject} key={idx} />
+                <div className="flex flex-col gap-4 lg:gap-6">
+                  {getUpcomingQuizzes().map((quizObject, idx) => {
+                    return <QuizCard quizObject={quizObject} key={idx} />;
                   })}
                 </div>
               }
             />
           </div>
-          <div ref={courseSectionRef} className="hidden flex flex-col gap-8 lg:flex lg:w-[65%]">
-            <Accordion sectionName={"Active Courses"} content=
-              {
-                <div className="flex flex-wrap gap-x-[4%] gap-y-6 md:gap-y-8">{
-                  activeCourseList.map((courseObject, idx) => {
-                    return <CourseCard courseObject={courseObject} toastMessageSet={toastMessageSet} key={idx} />
+          <div
+            ref={courseSectionRef}
+            className="hidden flex flex-col gap-8 lg:flex lg:w-[65%]"
+          >
+            <Accordion
+              sectionName={"Active Courses"}
+              content={
+                <div className="flex flex-wrap gap-x-[4%] gap-y-6 md:gap-y-8">
+                  {activeCourseList.map((courseObject, idx) => {
+                    return (
+                      <CourseCard
+                        courseObject={courseObject}
+                        toastMessageSet={toastMessageSet}
+                        key={idx}
+                      />
+                    );
                   })}
                 </div>
               }
             />
-            <Accordion collapesed sectionName={"Archived Courses"} content=
-              {
-                <div className="flex flex-wrap gap-x-[4%] gap-y-6 md:gap-y-8">{
-                  getArchivedCourses().map((courseObject, idx) => {
-                    return <CourseCard courseObject={courseObject} key={idx} />
+            <Accordion
+              collapesed
+              sectionName={"Archived Courses"}
+              content={
+                <div className="flex flex-wrap gap-x-[4%] gap-y-6 md:gap-y-8">
+                  {getArchivedCourses().map((courseObject, idx) => {
+                    return (
+                      <CourseCard courseObject={courseObject} key={idx} />
+                    );
                   })}
                 </div>
               }
@@ -149,5 +189,5 @@ export default function DashboardPage() {
         </main>
       </div>
     </>
-  )
+  );
 }
