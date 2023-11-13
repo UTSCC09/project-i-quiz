4bf468ac0ee3fd71714fe2c3a6190eaac34eb759
Archie Liu <archielhd@gmail.com>
2023-11-13 01:36:13 -0500

    Set image and answer optional (API)

--- server/models/OEQ.js
+++ server/models/OEQ.js
@@ -3,16 +3,15 @@ import mongoose from "mongoose";
 const OEQSchema = new mongoose.Schema({
   image: {
     type: String,
-    required: [true, "Please provide an image"]
   },
   prompt: {
     type: String,
-    required: [true, "Please provide a prompt"]
+    required: [true, "Please provide a prompt"],
   },
   maxLength: {
     type: Number,
-    required: [true, "Please provide max length"]
-  }
+    required: [true, "Please provide max length"],
+  },
 });
 
 const OEQ = mongoose.model("OEQ", OEQSchema);
