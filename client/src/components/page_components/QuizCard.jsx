a2e46ed282fb8223fe890f4014c88746952ef38e
Archie Liu <archielhd@gmail.com>
2023-11-12 20:32:10 -0500

    Add year number in quiz card

--- client/src/components/page_components/QuizCard.jsx
+++ client/src/components/page_components/QuizCard.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Badge from "components/elements/Badge.jsx";
 import colors from "tailwindcss/colors";
@@ -10,20 +9,26 @@ function getQuizState(quizObject) {
   const currentTime = new Date();
 
   if (startTime > currentTime) {
-    return "upcoming"
-  }
-  else if (endTime > currentTime) {
+    return "upcoming";
+  } else if (endTime > currentTime) {
     return "available";
-  }
-  else {
+  } else {
     return "closed";
   }
 }
 
 function getFormattedDateStr(time) {
-  const dateFormatOptions = { month: "short", day: "numeric" };
+  const dateFormatOptions = {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  };
   const timeFormatOptions = { hour: "numeric", minute: "numeric" };
-  return time.toLocaleDateString(undefined, dateFormatOptions) + " at " + time.toLocaleTimeString(undefined, timeFormatOptions);
+  return (
+    time.toLocaleDateString(undefined, dateFormatOptions) +
+    " at " +
+    time.toLocaleTimeString(undefined, timeFormatOptions)
+  );
 }
 
 // quizState: available, upcoming, closed
@@ -32,8 +37,27 @@ export default function QuizCard({ quizObject }) {
   const courseCode = quizObject.courseCode;
   const startTime = new Date(quizObject.endTime);
   const endTime = new Date(quizObject.endTime);
-  const colorList = [colors.pink[500], colors.rose[500], colors.red[500], colors.orange[500], colors.amber[500], colors.yellow[500], colors.lime[500], colors.green[500], colors.emerald[500], colors.teal[500], colors.cyan[500], colors.blue[500], colors.blue[600], colors.indigo[500], colors.violet[500], colors.purple[500]];
-  const accentColor = quizObject.accentColor || colorList[Math.floor(Math.random() * colorList.length)];
+  const colorList = [
+    colors.pink[500],
+    colors.rose[500],
+    colors.red[500],
+    colors.orange[500],
+    colors.amber[500],
+    colors.yellow[500],
+    colors.lime[500],
+    colors.green[500],
+    colors.emerald[500],
+    colors.teal[500],
+    colors.cyan[500],
+    colors.blue[500],
+    colors.blue[600],
+    colors.indigo[500],
+    colors.violet[500],
+    colors.purple[500],
+  ];
+  const accentColor =
+    quizObject.accentColor ||
+    colorList[Math.floor(Math.random() * colorList.length)];
   const submitted = quizObject.submitted;
 
   const quizState = getQuizState(quizObject);
@@ -61,20 +85,29 @@ export default function QuizCard({ quizObject }) {
   }
   return (
     <>
-      <Link to="/quiz/0" className="h-fit w-full rounded border-l-[12px] shadow shadow-gray-200 group cursor-pointer" style={{ borderLeftColor: accentColor, pointerEvents: isAvailable ? "auto" : "none", opacity: isAvailable ? 1 : 0.5 }}>
+      <Link
+        to="/quiz/0"
+        className="h-fit w-full rounded border-l-[12px] shadow shadow-gray-200 group cursor-pointer"
+        style={{
+          borderLeftColor: accentColor,
+          pointerEvents: isAvailable ? "auto" : "none",
+          opacity: isAvailable ? 1 : 0.5,
+        }}
+      >
         <div className="w-full py-4 md:py-6 h-fit bg-white group-hover:bg-gray-100 rounded-r border px-4 md:px-6 shrink-0 flex items-center transition-all">
           <div className="flex-col justify-center items-start inline-flex">
             <div className="items-center gap-2.5 inline-flex">
-              <div className="md:text-lg font-semibold">
-                {quizName}
-              </div>
-              {isAvailable && !submitted &&
-                <div className="w-2 h-2 shrink-0 rounded-full" style={{ backgroundColor: accentColor }}></div>
-              }
+              <div className="md:text-lg font-semibold">{quizName}</div>
+              {isAvailable && !submitted && (
+                <div
+                  className="w-2 h-2 shrink-0 rounded-full"
+                  style={{ backgroundColor: accentColor }}
+                ></div>
+              )}
               <Badge label={courseCode} accentColor={accentColor} />
-              {submitted &&
+              {submitted && (
                 <Badge label={"submitted"} accentColor={colors.green[600]} />
-              }
+              )}
             </div>
             <div className="text-gray-500 text-xs font-normal">
               {quizAvailabilityPrompt}
