cc44d768f44fad4da806da25914fd4788e877bd0
Archie Liu <archielhd@gmail.com>
2023-11-20 23:51:53 -0500

    Make expire time string conditional

--- server/utils/passwordResetUtils.js
+++ server/utils/passwordResetUtils.js
@@ -2,6 +2,19 @@ import { transporter, defaultMailOptions } from "./mailTransporter.js";
 import PASSWORD_RESET_CONSTANTS from "../constants/passwordResetConstants.js";
 
 const sendPasswordResetCode = async (user, code) => {
+  let expireTimeLengthStr;
+  if (PASSWORD_RESET_CONSTANTS.CODE_EXPIRATION / 1000 < 60) {
+    expireTimeLengthStr = `${
+      PASSWORD_RESET_CONSTANTS.CODE_EXPIRATION / 1000
+    } seconds`;
+  } else if (PASSWORD_RESET_CONSTANTS.CODE_EXPIRATION / 1000 == 60) {
+    expireTimeLengthStr = "1 minute";
+  } else {
+    expireTimeLengthStr = `${
+      PASSWORD_RESET_CONSTANTS.CODE_EXPIRATION / 1000 / 60
+    } minutes`;
+  }
+
   const mailOptions = {
     ...defaultMailOptions,
     to: user.email,
@@ -9,13 +22,15 @@ const sendPasswordResetCode = async (user, code) => {
     html: `
       <p>Reset your iQuiz password using the code below:</p>
       <h1>${code}</h1>
-      <p>The code will <strong>expire in ${PASSWORD_RESET_CONSTANTS.CODE_EXPIRATION / 1000} second(s)</strong><p>
-    `
+      <p>The code will <strong>expire in ${expireTimeLengthStr}</strong>.<p>
+    `,
   };
 
   try {
     await transporter.sendMail(mailOptions);
-    console.log("Password reset email sent to: " + user.email + " successfully!");
+    console.log(
+      "Password reset email sent to: " + user.email + " successfully!"
+    );
   } catch (error) {
     console.log("Failed to send password reset email", error);
   }
