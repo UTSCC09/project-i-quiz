3a9caab7b14ee56a47f47413ff00b84154fff86a
Archie Liu <archielhd@gmail.com>
2023-11-29 23:47:29 -0500

    QuizCard: Support response status

--- client/src/components/elements/Badge.jsx
+++ client/src/components/elements/Badge.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { CheckIcon } from "./SVGIcons";
+import { CheckIcon, FileEditIcon } from "./SVGIcons";
 
-export default function Badge({ accentColor = "#0366FF", label }) {
+export default function Badge({ accentColor = "#0366FF", label, iconId }) {
   return (
     <>
       <div
-        className="flex relative items-center h-fit w-fit px-1.5 font-medium py-0.5 rounded text-xs shrink-0"
+        className="flex relative items-center h-fit w-fit px-1.5 font-medium py-0.5 rounded text-xs shrink-0 gap-1"
         style={{ color: accentColor }}
       >
-        {label === "submitted" ? <CheckIcon className="h-3" /> : label}
+        {iconId && (
+          <>
+            {iconId === "submitted" && <CheckIcon className="h-3" />}
+            {iconId === "writing" && <FileEditIcon className="h-3" />}
+          </>
+        )}
+        {label}
         <div
           className="absolute w-full h-5 rounded opacity-10 -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2"
           style={{ backgroundColor: accentColor }}
--- client/src/components/page_components/QuizCard.jsx
+++ client/src/components/page_components/QuizCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Badge from "components/elements/Badge.jsx";
-import colors from "tailwindcss/colors";
+import colors, { inherit } from "tailwindcss/colors";
 import { Link } from "react-router-dom";
+import { isStudentUserType } from "utils/CookieUtils";
 
 function getQuizState(quizObject) {
   const startTime = new Date(quizObject.startTime);
@@ -37,7 +38,7 @@ export default function QuizCard({ accentColor = "#0366FF", quizObject }) {
   const courseCode = quizObject.courseCode;
   const startTime = new Date(quizObject.startTime);
   const endTime = new Date(quizObject.endTime);
-  const submitted = quizObject.submitted;
+  const responseStatus = quizObject.responseStatus;
   const quizState = getQuizState(quizObject);
   const startTimeStr = getFormattedDateStr(startTime);
   const endTimeStr = getFormattedDateStr(endTime);
@@ -71,19 +72,27 @@ export default function QuizCard({ accentColor = "#0366FF", quizObject }) {
           opacity: isAvailable ? 1 : 0.5,
         }}
       >
-        <div className="w-full py-4 md:py-6 h-20 md:h-24 bg-white group-hover:bg-gray-100 rounded-r border px-4 md:px-6 shrink-0 flex items-center transition-all">
+        <div
+          className="w-full py-4 md:py-6 h-20 md:h-24 bg-white group-hover:bg-gray-100 rounded-r border px-4 md:px-6 shrink-0 flex items-center transition-all"
+          style={{
+            borderColor: responseStatus === "writing" ? accentColor : inherit,
+            borderWidth: responseStatus === "writing" ? 1.5 : 1,
+          }}
+        >
           <div className="flex-col justify-center items-start inline-flex">
             <div className="items-center gap-2.5 inline-flex">
               <div className="md:text-lg font-semibold">{quizName}</div>
-              {isAvailable && !submitted && (
-                <div
-                  className="w-2 h-2 shrink-0 rounded-full"
-                  style={{ backgroundColor: accentColor }}
-                ></div>
-              )}
+              {isAvailable &&
+                isStudentUserType() &&
+                responseStatus !== "submitted" && (
+                  <div className="w-2 h-2 shrink-0 rounded-full bg-red-500"></div>
+                )}
               <Badge label={courseCode} accentColor={accentColor} />
-              {submitted && (
-                <Badge label={"submitted"} accentColor={colors.green[600]} />
+              {responseStatus === "submitted" && (
+                <Badge iconId="submitted" accentColor={colors.green[600]} />
+              )}
+              {responseStatus === "writing" && (
+                <Badge iconId="writing" accentColor={colors.gray[500]} />
               )}
             </div>
             <div className="text-gray-500 text-xs font-normal">
