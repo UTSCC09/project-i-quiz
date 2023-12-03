49efe7a9a80055e7a3bcde06e9057bac02986909
Archie Liu <archielhd@gmail.com>
2023-12-03 00:34:39 -0500

    Add quiz Info page

--- client/src/components/elements/SVGIcons.jsx
+++ client/src/components/elements/SVGIcons.jsx
@@ -219,6 +219,21 @@ function FileEditIcon({ className }) {
   );
 }
 
+function PenIcon({ className }) {
+  /* [Credit]: svg from https://www.flaticon.com/uicons */
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className={className}
+    >
+      <path d="M1.172,19.119A4,4,0,0,0,0,21.947V24H2.053a4,4,0,0,0,2.828-1.172L18.224,9.485,14.515,5.776Z" />
+      <path d="M23.145.855a2.622,2.622,0,0,0-3.71,0L15.929,4.362l3.709,3.709,3.507-3.506A2.622,2.622,0,0,0,23.145.855Z" />
+    </svg>
+  );
+}
+
 export {
   EllipsisIcon,
   ChevronIcon,
@@ -232,4 +247,5 @@ export {
   AdjustmentsIcon,
   PlusIcon,
   FileEditIcon,
+  PenIcon,
 };
--- client/src/pages/QuizInfoPage.jsx
+++ client/src/pages/QuizInfoPage.jsx
@@ -1,28 +1,49 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useParams } from "react-router";
-import { getQuiz } from "api/QuizApi";
-import { createQuizReponse, getQuizResponse } from "api/QuizResponseApi";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import Badge from "components/elements/Badge";
 import NavBar from "components/page_components/NavBar";
+import DropdownMenu from "components/elements/DropdownMenu";
 import { isStudentUserType } from "utils/CookieUtils";
+import Toast from "components/elements/Toast";
+import { fetchCourseObject } from "api/CourseApi";
+import { getQuiz } from "api/QuizApi";
+import { AdjustmentsIcon, PenIcon } from "components/elements/SVGIcons";
+import colors from "tailwindcss/colors";
+import {
+  createQuizReponse,
+  // generateQuizPDF,
+  getQuizResponse,
+} from "api/QuizResponseApi";
 
-const QuizInfoPage = () => {
-  const isStudent = isStudentUserType();
-  const { quizId } = useParams();
-  const [quizObject, quizObjectSet] = useState();
-  const [quizResponseSubmitted, setQuizResponseSubmitted] = useState(false);
+export default function QuizInfoPage() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { quizId } = useParams();
+  const { passInCourseObject } = useState() ?? {};
+  const [courseObject, courseObjectSet] = useState(passInCourseObject ?? null);
+  const [quizObject, quizObjectSet] = useState();
 
-  const onStartQuiz = (e) => {
-    e.preventDefault();
+  const [toastMessage, toastMessageSet] = useState();
 
+  const isStudent = isStudentUserType();
+
+  let quizOptions = [
+    {
+      label: "Download as PDF",
+      onClick: () => {
+        // generateQuizPDF(quizId);
+      },
+    },
+  ];
+  const [quizResponseStatus, quizResponseStatusSet] = useState(false);
+
+  const onStartQuiz = () => {
     const questionResponses = quizObject.questions.map((question) => {
       return {
         question: question._id,
         response: [""],
       };
     });
-
     createQuizReponse(quizId, questionResponses).then((payload) => {
       if (payload) {
         navigate("/quiz/" + quizId);
@@ -33,10 +54,23 @@ const QuizInfoPage = () => {
   };
 
   useEffect(() => {
-    if (!isStudent) {
-      navigate("/quiz/" + quizId);
-      return;
-    } else {
+    const { passInMessage } = location.state ?? "";
+    if (passInMessage) {
+      toastMessageSet(passInMessage);
+      navigate("", {});
+      setTimeout(() => {
+        toastMessageSet();
+      }, 3000);
+    }
+
+    getQuiz(quizId).then((quizPayload) => {
+      quizObjectSet(quizPayload);
+      fetchCourseObject(quizPayload.courseId).then((result) => {
+        courseObjectSet(result.payload);
+      });
+    });
+
+    if (isStudent) {
       getQuizResponse(quizId).then((result) => {
         if (
           !result.success &&
@@ -46,44 +80,104 @@ const QuizInfoPage = () => {
             quizObjectSet(payload);
           });
         } else if (result.success) {
-          if (result.payload.status === "submitted") {
-            setQuizResponseSubmitted(true);
-          } else {
-            navigate("/quiz/" + quizId);
-          }
+          quizResponseStatusSet(result.payload.status);
         } else {
           console.error(result.message);
           alert("Failed to fetch quiz response");
         }
       });
     }
-  }, [isStudent, navigate, quizId]);
+  }, [isStudent, location.state, navigate, quizId, toastMessageSet]);
 
   return (
     <>
+      <Toast toastMessage={toastMessage} toastMessageSet={toastMessageSet} />
       <NavBar />
-      <div className="min-h-screen w-full flex justify-center py-28 sm:py-36 bg-gray-100">
-        {quizResponseSubmitted ? (
-          <div className="flex flex-col items-center">
-            <h1 className="text-3xl font-bold">Quiz Submitted</h1>
-            <p className="text-lg">You have already submitted this quiz.</p>
+      <div className="min-h-screen w-full bg-gray-100 flex flex-col items-center py-36">
+        <main className="h-fit flex flex-col md:px-24 px-8 w-full lg:w-[64rem]">
+          <div className="flex flex-col gap-4 sm:flex-row sm:items-end justify-between mb-6 md:mb-8 h-28 sm:h-16">
+            <div className="flex flex-col pr-4">
+              {quizObject && (
+                <div className="flex items-center gap-3">
+                  <span className="text-gray-900 font-bold text-3xl md:text-4xl mb-1">
+                    {quizObject.quizName}
+                  </span>
+                  {isStudent && quizResponseStatus !== "submitted" && (
+                    <div className="w-2 h-2 shrink-0 rounded-full bg-red-500"></div>
+                  )}
+                  {courseObject && (
+                    <Badge
+                      label={
+                        courseObject.courseCode +
+                        " · " +
+                        courseObject.courseSemester
+                      }
+                      accentColor={courseObject.accentColor}
+                    />
+                  )}
+                  {((isStudent && quizResponseStatus === "writing") ||
+                    (!isStudent && quizObject.isDraft)) && (
+                    <Badge
+                      iconId={"writing"}
+                      label={"Draft"}
+                      accentColor={colors.gray[500]}
+                    />
+                  )}
+                  {isStudent && quizResponseStatus === "submitted" && (
+                    <Badge
+                      iconId={"submitted"}
+                      label={"Submitted"}
+                      accentColor={colors.green[500]}
+                    />
+                  )}
+                </div>
+              )}
+              {courseObject && (
+                <span className="text-gray-500 text-sm ml-0.5">
+                  {courseObject.courseCode}: {courseObject.courseName}
+                </span>
+              )}
+            </div>
+            <div className="flex gap-2 sm:gap-4 text-gray-700">
+              <div className="flex gap-2 sm:gap-4">
+                {!isStudent && quizObject && quizObject.isDraft && (
+                  <Link
+                    to={"/quiz/" + quizId}
+                    className="bg-white shadow-sm h-8 sm:h-10 w-8 sm:w-10 text-center rounded-md border cursor-pointer hover:bg-gray-100 flex items-center justify-center transition-all"
+                  >
+                    <PenIcon className="h-3" />
+                  </Link>
+                )}
+                <DropdownMenu
+                  buttonElement={
+                    <button className="bg-white shadow-sm h-8 sm:h-10 w-8 sm:w-10 text-center rounded-md border cursor-pointer hover:bg-gray-100 flex items-center justify-center transition-all">
+                      <AdjustmentsIcon className="h-[18px] sm:h-5" />
+                    </button>
+                  }
+                  options={quizOptions}
+                  menuAlignLeft
+                />
+                {isStudent && !quizResponseStatus && (
+                  <button
+                    className="bg-white shadow-sm font-semibold h-8 sm:h-10 px-4 text-sm text-center rounded-md border cursor-pointer hover:bg-gray-100 flex items-center justify-center transition-all"
+                    onClick={onStartQuiz}
+                  >
+                    Start Quiz
+                  </button>
+                )}
+                {isStudent && quizResponseStatus === "writing" && (
+                  <Link
+                    to={"/quiz/" + quizId}
+                    className="bg-white shadow-sm font-semibold h-8 sm:h-10 px-4 text-sm text-center rounded-md border cursor-pointer hover:bg-gray-100 flex items-center justify-center transition-all"
+                  >
+                    Continue Quiz
+                  </Link>
+                )}
+              </div>
+            </div>
           </div>
-        ) : (
-          <button
-            className="btn-primary w-fit text-sm px-8 py-2 mt-2 place-self-end"
-            type="button"
-            style={{
-              pointerEvents: quizObject ? "auto" : "none",
-              opacity: quizObject ? 1 : 0.5,
-            }}
-            onClick={(e) => onStartQuiz(e)}
-          >
-            Start Quiz
-          </button>
-        )}
+        </main>
       </div>
     </>
   );
-};
-
-export default QuizInfoPage;
+}
