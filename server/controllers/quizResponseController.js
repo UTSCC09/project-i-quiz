f358d214f6bdafa654d1bc010071776e607ace55
ericckzhou <ericckzhou@gmail.com>
2023-12-03 18:31:08 -0500

    Fix merge conflict issues

--- server/controllers/quizResponseController.js
+++ server/controllers/quizResponseController.js
@@ -447,7 +447,7 @@ export {
   editMyResponseForQuiz,
   getAllStudentResponsesForQuiz,
   submitMyResponseForQuiz,
-  generateQuizPDF,
+  getQuizResponse,
   gradeStudentQuizResponse,
   generateQuizPDF
 };
