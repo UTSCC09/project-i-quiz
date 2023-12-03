51eac088ec869fd0904192dcaaad2d4662986905
Archie Liu <archielhd@gmail.com>
2023-12-02 23:40:30 -0500

    Add student name and course code to email

--- server/utils/quizInvitationUtils.js
+++ server/utils/quizInvitationUtils.js
@@ -1,29 +1,49 @@
-import { transporter, defaultMailOptions, frontendUrl } from "./mailTransporter.js";
+import User from "../models/User.js";
+import {
+  transporter,
+  defaultMailOptions,
+  frontendUrl,
+} from "./mailTransporter.js";
 
 const sendQuizInvitation = async (course, emails, quiz) => {
-    if (!course || !quiz) {
-      console.log("Fail to provide arguments to send quiz invitations.");
+  if (!course || !quiz) {
+    console.error("Fail to provide arguments to send quiz invitations.");
+  }
+
+  for (const email of emails) {
+    const student = await User.findOne({ email: email });
+    if (!student) {
+      console.error("Cannot find user with email: " + email);
+      continue;
     }
-  
-    for (const email of emails) {
-      let quizInviteOptions = {
-        ...defaultMailOptions,
-        to: email,
-        subject: course.courseName + " - " + quiz.quizName + " Release Notice",
-        html: `
-          <p>Hi, ${quiz.quizName} has just been released!</p>
-          <p><b>${quiz.quizName}</b> will start on ${quiz.startTime.toUTCString()} and end on ${quiz.endTime.toUTCString()}!</p>
-          <p><a href=${frontendUrl + "quiz/" + quiz._id}>Click here to start!</p>
+    let quizInviteOptions = {
+      ...defaultMailOptions,
+      to: email,
+      subject: `[${course.courseCode} ${course.courseSemester}] ${quiz.quizName} Release Notice`,
+      html: `
+          <p>Hi ${student.firstName},</p>
+          <p>A new quiz has just been released!</p>
+          <p>Quiz Name: <b>${quiz.quizName}</b></p>
+          <p>Course: <b>${course.courseCode} ${course.courseSemester}</b> - ${
+        course.courseName
+      }</p>
+          <p>The quiz will start on <b>${quiz.startTime.toUTCString()}</b> and end on <b>${quiz.endTime.toUTCString()}</b>.</p>
+          <p>Be prepared and good luck!</p>
+          <p><a href=${
+            frontendUrl + "quiz/" + quiz._id
+          }>Click here to view the quiz!</p>
+          <p>Best,</p>
+          <p>iQuiz Team</p>
           <p>© iQuiz 2023. All rights reserved.</p>
-        `
-      };
-      
-      try {
-        await transporter.sendMail(quizInviteOptions);
-      } catch (err) { 
-        console.log("Fail to send quiz invitation email to: " + email);
-      }
+        `,
     };
+
+    try {
+      await transporter.sendMail(quizInviteOptions);
+    } catch (err) {
+      console.error("Fail to send quiz invitation email to: " + email);
+    }
+  }
 };
 
 export default sendQuizInvitation;
