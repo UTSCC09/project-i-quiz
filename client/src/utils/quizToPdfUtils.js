180251067b1eb620de066c956f5a848ccca99be7
Archie Liu <archielhd@gmail.com>
2023-12-02 23:48:06 -0500

    Reformat only

--- client/src/utils/quizToPdfUtils.js
+++ client/src/utils/quizToPdfUtils.js
@@ -1,83 +1,98 @@
 import { jsPDF } from "jspdf";
 
 function generatePDF(course, quiz, questions, user, quizResponse) {
-    if (!course || !quiz || !questions || !user || !quizResponse) {
-        return;
-    }
-    const pdf = new jsPDF();
+  if (!course || !quiz || !questions || !user || !quizResponse) {
+    return;
+  }
+  const pdf = new jsPDF();
 
-    // Title
-    pdf.setFontSize(18);
-    pdf.setFont(undefined, "bold")
+  // Title
+  pdf.setFontSize(18);
+  pdf
+    .setFont(undefined, "bold")
     .text(`${course.courseName} - ${quiz.quizName}`, 20, 15)
     .setFont(undefined, "normal");
 
-    // Student details
-    pdf.setFontSize(14);
-    pdf.setFont(undefined, "bold")
+  // Student details
+  pdf.setFontSize(14);
+  pdf
+    .setFont(undefined, "bold")
     .text(`Student: ${user.firstName} ${user.lastName}`, 20, 30)
     .setFont(undefined, "normal");
 
-    // Date
-    pdf.setFontSize(12);
-    const start = new Date(quiz.startTime);
-    const end = new Date(quiz.endTime);
-    pdf.text(`Start Date: ${start.toUTCString()}`, 20, 40);
-    pdf.text(`End Date: ${end.toUTCString()}`, 20, 50);
+  // Date
+  pdf.setFontSize(12);
+  const start = new Date(quiz.startTime);
+  const end = new Date(quiz.endTime);
+  pdf.text(`Start Date: ${start.toUTCString()}`, 20, 40);
+  pdf.text(`End Date: ${end.toUTCString()}`, 20, 50);
 
-    // Setting Y-Position
-    let yPos = 65;
-    
-    // Question index
-    let index = 0;
+  // Setting Y-Position
+  let yPos = 65;
 
-    let finalScore = 0;
-    let totalScore = 0;
+  // Question index
+  let index = 0;
 
-    // Questions
-    for (const questionResponse of quizResponse.questionResponses) {
-        const question = questions.filter((q) => q._id.toString() === questionResponse.question.toString())[0];
-        if (question) { 
-            const response = questionResponse.response ? questionResponse.response.join(', ') : "N/A";
-            const score = questionResponse.score ? questionResponse.score : 0;
-            const maxScore = question.maxScore ? question.maxScore : 0;
-            
-            pdf.setFontSize(12);
-            pdf.setFont(undefined, "bold")
-            .text(`Question ${index + 1}: ${question.prompt ? question.prompt : "N/A"}`, 20, yPos)
-            .setFont(undefined, "normal");
-            pdf.setFontSize(11);
+  let finalScore = 0;
+  let totalScore = 0;
 
-            if (question.type == 'MSQ' || question.type == 'MCQ') {
-                let answers = [];
+  // Questions
+  for (const questionResponse of quizResponse.questionResponses) {
+    const question = questions.filter(
+      (q) => q._id.toString() === questionResponse.question.toString()
+    )[0];
+    if (question) {
+      const response = questionResponse.response
+        ? questionResponse.response.join(", ")
+        : "N/A";
+      const score = questionResponse.score ? questionResponse.score : 0;
+      const maxScore = question.maxScore ? question.maxScore : 0;
 
-                for (const r of questionResponse.response) {
-                    answers.push(question.choices[parseInt(r)] 
-                    ? question.choices[parseInt(r)].content : "N/A");
-                }
+      pdf.setFontSize(12);
+      pdf
+        .setFont(undefined, "bold")
+        .text(
+          `Question ${index + 1}: ${
+            question.prompt ? question.prompt : "N/A"
+          }`,
+          20,
+          yPos
+        )
+        .setFont(undefined, "normal");
+      pdf.setFontSize(11);
 
-                pdf.text(`You selected: ${answers}`, 20, yPos + 15);
+      if (question.type === "MSQ" || question.type === "MCQ") {
+        let answers = [];
 
-            } 
-            else {
-                pdf.text(`You answered: ${response}`, 20, yPos + 15);
-            }
-            pdf.text(`You scored: ${score}`, 20, yPos + 25);
-
-            yPos += 40; 
-            index += 1;
-            finalScore += score;
-            totalScore += maxScore;
+        for (const r of questionResponse.response) {
+          answers.push(
+            question.choices[parseInt(r)]
+              ? question.choices[parseInt(r)].content
+              : "N/A"
+          );
         }
-    }
 
-    // Final Score
-    yPos += 5;
-    pdf.setFontSize(14);
-    pdf.setFont(undefined, "bold")
+        pdf.text(`You selected: ${answers}`, 20, yPos + 15);
+      } else {
+        pdf.text(`You answered: ${response}`, 20, yPos + 15);
+      }
+      pdf.text(`You scored: ${score}`, 20, yPos + 25);
+
+      yPos += 40;
+      index += 1;
+      finalScore += score;
+      totalScore += maxScore;
+    }
+  }
+
+  // Final Score
+  yPos += 5;
+  pdf.setFontSize(14);
+  pdf
+    .setFont(undefined, "bold")
     .text(`Final Score: ${finalScore}/${totalScore}`, 20, yPos);
 
-    return pdf;
+  return pdf;
 }
 
 export default generatePDF;
